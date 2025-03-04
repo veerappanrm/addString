924781c5981a3d6afab39e1e0d1780f2380c8a85
veerappanrm <srmveera@gmail.com>
2025-03-04 10:29:11 +0530

    delimiters

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-
+import re
 import unittest
 
 def add_function(numbers):
-    nums = numbers.split(',')
+    nums = re.split(';|,|\n|\\\D|\/\D', numbers)
     sum = 0
     for num in nums:
         try:
@@ -26,6 +26,16 @@ class TestAddFunction(unittest.TestCase):
     def test_add_multiple_value(self):
         expected_result = 5
         result = add_function('2,3')
+        self.assertEqual(result, expected_result)
+
+    def test_add_with_newline(self):
+        expected_result = 6
+        result = add_function('1\n2,3')
+        self.assertEqual(result, expected_result)
+
+    def test_add_with_multiple_delimiter(self):
+        expected_result = 3
+        result = add_function('//;\n1;2')
         self.assertEqual(result, expected_result)
 
 if __name__ == '__main__':
